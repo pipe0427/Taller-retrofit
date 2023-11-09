2f183a96f9a3a14831ffceeb660e73345a10dc18
sebastian <sebazgrisales@gmail.com>
2023-11-08 22:30:55 -0500

    listo

---
+++ app/src/main/java/com/example/myapplication/MyAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(private val posts: List<Post>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_post, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val post = posts[position]
+        holder.bind(post)
+    }
+
+    override fun getItemCount(): Int {
+        return posts.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+
+        fun bind(post: Post) {
+            titleTextView.text = post.title
+        }
+    }
+}
